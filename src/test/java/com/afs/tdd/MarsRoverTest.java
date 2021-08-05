170358576d091b8d1782336006ec2c0ab2cdb3eb
ADRIAPA <ADRIAPA@OOCL.COM>
2021-08-05 17:33:27 +0800

    [ST004-02][ADRIAPA]test: Added test scenario for move left

--- src/test/java/com/afs/tdd/MarsRoverTest.java
+++ src/test/java/com/afs/tdd/MarsRoverTest.java
@@ -5,13 +5,27 @@ import org.junit.jupiter.api.Test;
 
 class MarsRoverTest {
     @Test
-    void should_return_locationX_0_locationY_direction1_N_when_execute_command_given_0_0_N_and_M() {
+    void should_return_locationX_0_locationY_direction_N_when_execute_command_given_0_0_N_and_M() {
         //given
         MarsRover marsRover = new MarsRover(new RoverStatus(0, 0, "N"));
         RoverStatus expectedRoverStatus = new RoverStatus(0, 1, "N");
         //when
         marsRover.executeCommand("M");
 
+        //then
+        Assertions.assertEquals(expectedRoverStatus.getLocationX(), marsRover.getRoverStatus().getLocationX());
+        Assertions.assertEquals(expectedRoverStatus.getLocationY(), marsRover.getRoverStatus().getLocationY());
+        Assertions.assertEquals(expectedRoverStatus.getDirection(), marsRover.getRoverStatus().getDirection());
+    }
+
+    @Test
+    void should_return_locationX_0_locationY_direction_W_when_execute_command_given_0_0_N_and_L() {
+        //given
+        MarsRover marsRover = new MarsRover(new RoverStatus(0, 0, "N"));
+        RoverStatus expectedRoverStatus = new RoverStatus(0, 0, "W");
+        //when
+        marsRover.executeCommand("L");
+        //then
         //then
         Assertions.assertEquals(expectedRoverStatus.getLocationX(), marsRover.getRoverStatus().getLocationX());
         Assertions.assertEquals(expectedRoverStatus.getLocationY(), marsRover.getRoverStatus().getLocationY());
