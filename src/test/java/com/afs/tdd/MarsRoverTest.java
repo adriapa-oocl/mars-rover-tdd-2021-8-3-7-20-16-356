3c06dcb877312139558c1e697161d1a89347b4fe
ADRIAPA <ADRIAPA@OOCL.COM>
2021-08-05 18:02:20 +0800

    [ST004-02][ADRIAPA]test: Added test scenario for move left from South direction

--- src/test/java/com/afs/tdd/MarsRoverTest.java
+++ src/test/java/com/afs/tdd/MarsRoverTest.java
@@ -55,4 +55,17 @@ class MarsRoverTest {
         Assertions.assertEquals(expectedRoverStatus.getLocationY(), marsRover.getRoverStatus().getLocationY());
         Assertions.assertEquals(expectedRoverStatus.getDirection(), marsRover.getRoverStatus().getDirection());
     }
+
+    @Test
+    void should_return_locationX_0_locationY_0_direction_E_when_execute_command_given_0_0_S_and_L() {
+        //given
+        MarsRover marsRover = new MarsRover(new RoverStatus(0, 0, "S"));
+        RoverStatus expectedRoverStatus = new RoverStatus(0, 0, "E");
+        //when
+        marsRover.executeCommand("L");
+        //then
+        Assertions.assertEquals(expectedRoverStatus.getLocationX(), marsRover.getRoverStatus().getLocationX());
+        Assertions.assertEquals(expectedRoverStatus.getLocationY(), marsRover.getRoverStatus().getLocationY());
+        Assertions.assertEquals(expectedRoverStatus.getDirection(), marsRover.getRoverStatus().getDirection());
+    }
 }
