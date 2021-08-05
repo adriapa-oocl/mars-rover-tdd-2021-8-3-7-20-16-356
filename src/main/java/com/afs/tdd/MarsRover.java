c53044b592580416ca7758dafa0bc66425e28379
ADRIAPA <ADRIAPA@OOCL.COM>
2021-08-05 17:30:05 +0800

    [ST004-02][ADRIAPA]feat: implemented move scenario to update location y coordinate

--- src/main/java/com/afs/tdd/MarsRover.java
+++ src/main/java/com/afs/tdd/MarsRover.java
@@ -4,16 +4,22 @@ public class MarsRover {
     private RoverStatus roverStatus;
 
     public MarsRover(RoverStatus roverStatus) {
-
         this.roverStatus = roverStatus;
     }
 
 
     public void executeCommand(String command) {
-
+        if (command.equals("M")){
+            move();
+        }
     }
 
-
+    private void move() {
+        final int locationX = roverStatus.getLocationX();
+        final int locationY = roverStatus.getLocationY();
+        final String direction = getRoverStatus().getDirection();
+        roverStatus = new RoverStatus(locationX, locationY+1, direction);
+    }
 
 
     public RoverStatus getRoverStatus() {
